09d92b271489d60f48df62ca26c1848a316c1f25
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2019-12-19 15:50:29 -0500

    cmake: dedupe with foreach

--- src/basic.f90
+++ src/basic.f90
@@ -4,7 +4,7 @@ real :: tau
 
 interface
 module real elemental function pi2tau(pi)
-  real, intent(in) :: pi
+real, intent(in) :: pi
 end function pi2tau
 end interface
 
@@ -15,6 +15,7 @@ end module mother
 
 program hier1
 use mother
+implicit none
 
 tau = pi2tau(pi)
 
