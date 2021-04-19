0f8d7a3b068337f6fe59ca0162d1379e60c772be
Michael Hirsch <scivision@users.noreply.github.com>
2021-04-18 22:08:37 -0400

    add same name example

--- src/repeated.f90
+++ src/repeated.f90
@@ -16,6 +16,23 @@ end interface
 end module parent
 
 
+submodule (parent) child
+
+implicit none
+
+contains
+
+module integer function intf()
+intf = 2
+end function intf
+
+module pure integer function pintf()
+pintf = 3
+end function pintf
+
+end submodule child
+
+
 program cmake_check
 use parent, only : intf, pintf
 implicit none
